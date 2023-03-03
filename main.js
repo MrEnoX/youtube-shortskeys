0d1bf363403c6fb66afbd01d9dc697aa7a4cc056
MrEnoX <97337724+MrEnoX@users.noreply.github.com>
2023-03-03 22:21:28 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube ShortsKeys
 // @namespace    https://github.com/MrEnoX/
-// @version      1.0.0
+// @version      1.1.0
 // @description  Adds ArrowRight and ArrowLeft keys to YouTube Shorts same as ArrowUp and ArrowDown!
 // @author       MrEnoX
 // @homepage     https://github.com/MrEnoX/youtube-shortskeys/
@@ -10,14 +10,21 @@
 // @match        *://*.youtube-nocookie.com/*
 // @icon         https://raw.githubusercontent.com/MrEnoX/youtube-shortskeys/main/icon.png
 // @grant        none
+// @license      MIT
+// @compatible   chrome
+// @compatible   edge
+// @compatible   firefox
+// @compatible   opera
+// @compatible   safari
 // ==/UserScript==
-
+ 
 console.log("YouTube ShortsKeys")
-
+ 
 //for key codes look here: https://www.toptal.com/developers/keycode
+// HERE IS FOR CHANGING THE KEYS YOU WANT TO USE
 const upkey = "ArrowLeft";
 const downkey = "ArrowRight";
-
+ 
 document.addEventListener("keydown", function(event) {
   if (event.code === upkey) {
     const shortsContainer = document.querySelector("#shorts-container");
@@ -27,7 +34,7 @@ document.addEventListener("keydown", function(event) {
     }
   }
 });
-
+ 
 document.addEventListener("keyup", function(event) {
   if (event.code === downkey) {
     const shortsContainer = document.querySelector("#shorts-container");
